9699f33db55e4a879ae14ec9ece39791d19900b8
Soren Christensen <sorenchristensen@gmail.com>
2015-04-19 17:57:29 -0600

    Revert "Additional work on Examples.R"
    This reverts commit 455f44d21f9165f04dd149dc29f36fdf6cfa51e8.

--- Examples.R
+++ Examples.R
@@ -1,38 +1,17 @@
-library(adehabitatHR)
-library(adehabitatHS)
-library(adehabitatLT)
-library(adehabitatMA)
-
-library(dplyr)
+library(adehabitat)
+library(ellipse)
 
 track = read.csv("DeerSample.csv")
 track = na.omit(track)
 
-track = filter(track, DATETIME >= 40603 & DATETIME <= 40633)
-
 summary(track)
 head(track)
 
-trackSP = SpatialPoints(data.frame(track$LONGITUDE, track$LATITUDE))
+coordinates(track) = c("LONGITUDE", "LATITUDE")
+class(track)
 
-trackCLU = clusthr(trackSp)
+plot(track)
 
-plot(trackCLU)
+?mcp
 
-trackCP = mcp(trackSP, percent = 90)
-plot(trackCP)
-plot(trackSP, add = T)
-
-HRs = mcp.area(trackSP, percent = seq(50, 100, by = 5))
-
----------------
-  
-trackKUD = kernelUD(trackSP)
-image(trackKUD)
-
-trackKudl = kernelUD(trackSP, h = "LSCV")
-image(trackKudl)
-plotLSCV(trackKudl)
-
-trackHr = getverticeshr(trackKUD)
-plot(trackHr,col = 3)
+demo(homerange)
